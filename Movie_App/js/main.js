e2f1cb2feec82cdffc8142fc0215982eeef9781c
Yhomi <kareemyomi91@gmail.com>
2020-04-02 10:16:48 +0100

    displaying fetched data

--- Movie_App/js/main.js
+++ Movie_App/js/main.js
@@ -43,6 +43,40 @@ function getMovie() {
     .get("http://www.omdbapi.com/?i=" + movieId + "&apikey=ad7d9e69")
     .then(response => {
       console.log(response);
+      let movie = response.data;
+      let output = `
+        <div class="row">
+          <div class="col-md-4">
+            <img src="${movie.Poster}" class="thumbnail">
+          </div>
+          <div class="col-md-8">
+            <h2>${movie.Title}</h2>
+            <ul class="list-group">
+              <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
+              <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
+              <li class="list-group-item"><strong>Language:</strong> ${movie.Language}</li>
+              <li class="list-group-item"><strong>Runtime:</strong> ${movie.Runtime}</li>
+              <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
+              <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
+              <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
+              <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
+              <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
+            </ul>
+          </div>
+        </div>
+        <br>
+        <div class="row">
+          <div class="well">
+            <h3>Plot</h3>
+            ${movie.Plot}
+            <hr>
+            <a href="http://www.imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View iMDB</a>
+            <a href="index.html" class="btn btn-secondary">Go Back To Search</a>
+          </div>
+        </div>
+        
+      `;
+      $("#movie").html(output);
     })
     .catch(err => {
       console.log(err);
